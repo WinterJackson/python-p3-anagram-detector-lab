124d7abf0eeb72ba0597ffead9bb61f27c0d38bc
WinterJackson <winterjacksonwj@gmail.com>
2023-08-27 01:08:35 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, anagram_list):
+        matched_anagrams = []
+        
+        for candidate in anagram_list:
+            candidate_lower = candidate.lower()
+            
+            if candidate_lower != self.word and sorted(candidate_lower) == sorted(self.word):
+                matched_anagrams.append(candidate)
+        
+        return matched_anagrams
